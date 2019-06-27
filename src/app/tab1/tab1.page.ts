8be1c92889bfb25a89e092336e74f9557c8997c1
Andreas Diehl <adiehl.developer@bitquadrat.de>
2019-06-27 16:49:27 +0200

    Getting c0.25 from the Web

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -10,5 +11,11 @@ export class Tab1Page {
   public title: string = 'Titel';
   public description: string = 'Beschreibung';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.http.get('https://cloud.bitquadrat.de/noname.php', {responseType: 'text'}).subscribe((data) => {
+      const dates = data.match(/Treffen<\/h1>(.*)<\/div>/s);
+      this.title = 'Chaotische Viertelstunden';
+      this.description = dates.toString();
+    });
+  }
 }
